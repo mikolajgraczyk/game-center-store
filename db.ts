8276264d59f8d168c655593dfb4e966f490bcec2
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-10-05 15:41:13 +0200

    installed mongodb package, created schema for games and basic API

---
+++ db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connect = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    }catch(error){
+        throw new Error("Error in connecting to mongodb")
+    }
+};
+
+export default connect;
