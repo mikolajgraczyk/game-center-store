532f3553a523add5c8f64cf0df71482a84c25b0f
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-11-16 14:06:04 +0100

    formatted code

--- src/app/[locale]/homePage/page.tsx
+++ src/app/[locale]/homePage/page.tsx
@@ -9,9 +9,7 @@ const HomePage = () => {
   const { isLoading, isError, data } = useQuery({ queryKey: ['games'], queryFn: fetchGames });
 
   if (isLoading) console.log('Ładowanie');
-
   if (data) console.log(data);
-
   if (isError) console.log('Błąd');
 
   return <p>{t('Home Page')}</p>;
