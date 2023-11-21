3832772a40d1fca2c25baed56dcb446c3e6fc243
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-11-21 15:08:10 +0100

    code fixed

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -1,15 +1,12 @@
-import mongoose, { ConnectOptions } from 'mongoose';
+import mongoose from 'mongoose';
 
 const connect = async () => {
- try{
-  await mongoose.connect(process.env.MONGO_URL as string, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as ConnectOptions);
-  console.log("mongo connection successful");
- }catch(error){
-  throw new Error("Error in connectiong to mongodb");
- }
+  try {
+    await mongoose.connect(process.env.MONGO_URL as string);
+    console.log('mongo connection successful');
+  } catch (error) {
+    throw new Error('Error in connecting to mongodb');
+  }
 };
 
 export default connect;
