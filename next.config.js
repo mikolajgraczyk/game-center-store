b6201c7bfc23c4365e7ceb1079a817d55beacd49
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-11-30 17:53:48 +0100

    disable eslint for module.exports as it cannot be exported as default

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
   },
 };
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNextIntl = require('next-intl/plugin')('src/i18n.ts');
 
 module.exports = withNextIntl(nextConfig);
