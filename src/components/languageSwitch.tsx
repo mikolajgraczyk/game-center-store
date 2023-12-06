2f075abc5a10236bd98eb701f187e610d61e601c
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-12-06 16:23:51 +0100

    code fixes

--- src/components/languageSwitch.tsx
+++ src/components/languageSwitch.tsx
@@ -1,24 +1,22 @@
 'use client';
+
 import { useLocale } from 'next-intl';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useTransition } from 'react';
 import { useRouter, usePathname } from 'next-intl/client';
-import { useTransition } from 'react';
 import LOCALES from '@/constants/locales';
 import Select from './select';
 
 function LanguageSwitch() {
-  const [isPending, startTransition] = useTransition();
+  const [, startTransition] = useTransition();
 
   const router = useRouter();
   const pathname = usePathname();
   const locale = useLocale();
 
-  const localesOptions = LOCALES.map((option) => {
-    return {
-      key: option,
-      value: option,
-    };
-  });
+  const localesOptions = LOCALES.map((option) => ({
+    key: option,
+    value: option,
+  }));
 
   const handleSelect = ({ target }: ChangeEvent<HTMLSelectElement>) => {
     const lang = target.value;
