2dc8f4b14741c3a038c696e7d89f16887abe89a3
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-12-06 14:54:02 +0100

    API endpoints based on the environment

--- src/scripts/fetchGames.ts
+++ src/scripts/fetchGames.ts
@@ -1,7 +1,12 @@
 import { notFound } from 'next/dist/client/components/not-found';
 
+const isDev = process.env.NODE_ENV === 'development';
+const apiUrl = isDev
+  ? 'http://localhost:3000/api/games'
+  : 'game-center-store-c9e37hssb-mikolajgraczyk.vercel.app/api/games';
+
 const fetchGames = async () => {
-  const res = await fetch('http://localhost:3000/api/games', { cache: 'no-store' });
+  const res = await fetch(apiUrl, { cache: 'no-store' });
 
   if (!res.ok) {
     return notFound();
