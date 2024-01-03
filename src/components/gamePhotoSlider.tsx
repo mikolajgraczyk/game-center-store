b8dd010df1ff73ca528c2e245aa36b9b39dcde73
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2024-01-03 14:59:48 +0100

    slider movement validation

--- src/components/gamePhotoSlider.tsx
+++ src/components/gamePhotoSlider.tsx
@@ -9,6 +9,20 @@ interface IGamePhotoSlider {
 
 function GamePhotoSlider({ photos }: IGamePhotoSlider) {
   const [selectedPhotoIndex, setSelectedPhotoIndex] = useState(0);
+  const [sliderPosition, setSliderPosition] = useState(0);
+
+  function moveSlider(isForward: boolean) {
+    const sliderMaxPosition = photos.length / 5;
+
+    if (isForward) {
+      setSliderPosition((prev) => (prev + 1 >= sliderMaxPosition ? 0 : prev + 1));
+      return;
+    }
+    if (sliderMaxPosition === 1) {
+      return;
+    }
+    setSliderPosition((prev) => (prev - 1 < 0 ? Math.floor(sliderMaxPosition) : prev - 1));
+  }
 
   return (
     <div className="w-full">
@@ -35,36 +49,48 @@ function GamePhotoSlider({ photos }: IGamePhotoSlider) {
         <button
           type="button"
           className="bg-buttons-slider p-[8px] self-center rounded-full hover:bg-buttons-slider/50 transition duration-200"
+          onClick={() => moveSlider(false)}
         >
           <div className="rotate-180">
             <Icon name="sliderArrow" />
           </div>
         </button>
         <div className="flex w-full max-w-[647px] overflow-hidden">
-          <motion.div className="flex gap-[15px] w-full" animate={{ translateX: `-${0 * 100}%` }}>
-            {photos.map((photoURL, index) => (
-              <button
-                type="button"
-                onClick={() => setSelectedPhotoIndex(index)}
-                className="h-full aspect-video relative opacity-40 hover:opacity-100 transition duration-300"
-                key={photoURL}
-              >
-                <Image
-                  src={photoURL}
-                  alt="Preview Photo"
-                  className="object-cover h-full w-full rounded-[4px]"
-                  fill
-                  quality={1}
-                  priority
-                  unoptimized
-                />
-              </button>
-            ))}
+          <motion.div
+            className="flex gap-[15px] w-full"
+            animate={{ translateX: `-${sliderPosition * 100}%` }}
+            transition={{ type: 'tween', duration: 0.2 }}
+          >
+            {photos.map((photoURL, index) => {
+              const isSelected = index === selectedPhotoIndex;
+
+              return (
+                <button
+                  type="button"
+                  onClick={() => setSelectedPhotoIndex(index)}
+                  className={`h-full aspect-video relative ${
+                    isSelected ? `opacity-100` : `opacity-40`
+                  } hover:opacity-100 transition duration-300`}
+                  key={photoURL}
+                >
+                  <Image
+                    src={photoURL}
+                    alt="Preview Photo"
+                    className="object-cover rounded-[4px]"
+                    fill
+                    quality={1}
+                    priority
+                    unoptimized
+                  />
+                </button>
+              );
+            })}
           </motion.div>
         </div>
         <button
           type="button"
           className="bg-buttons-slider p-[8px] self-center rounded-full hover:bg-buttons-slider/50 transition duration-200"
+          onClick={() => moveSlider(true)}
         >
           <Icon name="sliderArrow" />
         </button>
