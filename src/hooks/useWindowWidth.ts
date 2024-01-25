f1ba12c49fce54e27c00e734c0e2b404a301e0b1
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2024-01-25 13:11:35 +0100

    add event listener cleanup for preventing memory leaks

--- src/hooks/useWindowWidth.ts
+++ src/hooks/useWindowWidth.ts
@@ -4,9 +4,15 @@ function useWindowWidth() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    window.addEventListener('resize', () => {
+    const handleResize = () => {
       setWindowWidth(window.innerWidth);
-    });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
 
   return windowWidth;
